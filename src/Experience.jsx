0244782f6192ef2268dfe7fb53b612105b5a2dfa
tim <dzhoroev1@gmail.com>
2023-02-20 17:07:51 +0900

    feat: box component

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,15 +1,20 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+import Box from './comp/Box'
 
 export default function Experience()
 {
     return <>
+        <Perf position="top-left"/>  
+        <OrbitControls />  
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
 
-        <OrbitControls makeDefault />
-        
-        <mesh>
-            <boxGeometry />
-            <meshNormalMaterial />
-        </mesh>
-
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
     </>
 }
